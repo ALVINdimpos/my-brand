cb3689296c028e53ee458373fa4874befa7e3b3e
ALVINdimpos <alvindimpos@gmail.com>
2023-03-20 12:44:56 +0200

    Clean up project directory and remove unnecessary files
    This commit includes the following changes:
    - Removed all temporary files and directories that were no longer needed
    - Deleted unused libraries and dependencies that were not being used by the project
    - Cleared out the pycache directory to remove compiled Python files
    - Removed any other files that were not necessary for the project
    
    By removing these unnecessary files, we can improve the overall cleanliness and organization of the project directory. This should make it easier for developers to navigate the codebase and avoid any potential conflicts or confusion.
    
    Overall, this commit should have no impact on the functionality of the project, but it will help to streamline future development efforts and ensure that the project remains easy to work with.

--- JavaScript/viewBlog.js
+++ JavaScript/viewBlog.js
@@ -9,66 +9,62 @@ const year = date.getFullYear();
 const currentDate = `${day}/${month}/${year}`;
 
 const renderBlog = async () => {
-  const res = await fetch(`${URL}/blog/${id}`);
-  const blog = await res.json();
-  // Fetch comments for the corresponding blog post ID
-  const commentsRes = await fetch(`${URL}/blog/${id}/comments`);
-  const comments = await commentsRes.json();
-  let template = "";
-  template += `
-		  <header>
-		  <h1>My Blog</h1>
-		  <p>By ${blog.author} | ${blog.createdAt}</p>
+	const res = await fetch(`${URL}/blog/${id}`);
+	const blog = await res.json();
+	// Fetch comments for the corresponding blog post ID
+	const commentsRes = await fetch(`${URL}/blog/${id}/comments`);
+	const comments = await commentsRes.json();
+
+	let template = "";
+	template += `
+	  <header>
+		<h1>My Blog</h1>
+		<p>By ${blog.author} | ${blog.createdAt}</p>
 	  </header>
 	  <main>
-		  <article>
-			  <h2>${blog.title}</h2>
-			  <img src="${blog.image}" alt="Blog Image">
-			  <p>${blog.content.replace(/\n/g, "<br>")}</p>
-
-			  <div class="like">
-				  <button id="like-btn">Like</button>
-				  <p id="like-count">${blog.likes} likes</p>
-			  </div>
-			  <div class="comment">
-				  <h3>Comments</h3>
-				  <form>
-					  <label for="comment">Comment:</label>
-					  <textarea id="comment" name="comment" required></textarea>
-					  <button type="button" onclick="postComment('${blog._id}')">Post</button>
-				  </form>
-				  <article>
-				  <ul class="comments">
-					${comments
-            .map(
-              (comment) => `
-					  <li class="comment">
-						<div class="comment-header">
-						  <img src="../Images/user.png" alt="User Avatar">
-						  <div class="comment-meta">
-							<h3 class="comment-author">anonymous</h3>
-							<time class="comment-time">${currentDate}</time>
-						  </div>
-						  <div class="comment-actions">
-							<button class="like-button">Like</button>
-							<button class="reply-button">Reply</button>
-						  </div>
-						</div>
-						<p class="comment-text">${comment.commentBody}</p>
-					  </li>
-					`
-            )
-            .join("")}
-				  </ul>
-				</article>
-				  
-				  </div>
-			  </div>
-		  </article>
+		<article>
+		  <h2>${blog.title}</h2>
+		  <img src="${blog.image}" alt="Blog Image">
+		  <div style="white-space: pre-wrap">${blog.content.replace(/\n/g, '<br>')}</div>
+		  <div class="like">
+			<button id="like-btn">Like</button>
+			<p id="like-count">${blog.likes} likes</p>
+		  </div>
+		  <div class="comment">
+			<h3>Comments</h3>
+			<form>
+			  <label for="comment">Comment:</label>
+			  <textarea id="comment" name="comment" required></textarea>
+			  <button type="button" onclick="postComment('${blog._id}')">Post</button>
+			</form>
+			<article>
+			  <ul class="comments">
+				${comments.map((comment) => `
+				  <li class="comment">
+					<div class="comment-header">
+					  <img src="../Images/user.png" alt="User Avatar">
+					  <div class="comment-meta">
+						<h3 class="comment-author">anonymous</h3>
+						<time class="comment-time">${currentDate}</time>
+					  </div>
+					  <div class="comment-actions">
+						<button class="like-button">Like</button>
+						<button class="reply-button">Reply</button>
+					  </div>
+					</div>
+					<p class="comment-text">${comment.commentBody}</p>
+				  </li>
+				`).join("")}
+			  </ul>
+			</article>
+		  </div>
+		</article>
 	  </main>
-	  `;
-  blogTitle.innerHTML = template;
-};
+	`;
+	
+	blogTitle.innerHTML = template;
+  };
+  
 
 // post comment
 const postComment = async (id) => {
